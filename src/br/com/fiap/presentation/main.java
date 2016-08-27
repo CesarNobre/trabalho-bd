62ba0e0a51117c0dc8302d7d684839af16f82f0a
cesarnobre <cesarnobrefilho@gmail.com>
2016-08-26 22:44:41 -0300

    more changes

--- src/br/com/fiap/presentation/main.java
+++ src/br/com/fiap/presentation/main.java
@@ -1,16 +1,24 @@
 package br.com.fiap.presentation;
 
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.StoredProcedureQuery;
+import javax.persistence.TypedQuery;
+
+import br.com.fiap.singleton.EMSingleton;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CallableStatementnt call = connection.prepareCall("{ ? = call MYSCHEMA.MYFUNC(?,?) }");
-	    call.registerOutParameter( 1, Types.INTEGER ); // or whatever it is
-	    call.setLong(2, id);
-	    call.setLong(3, transId);
-	    call.execute();
-	    int result = call.getInt(1);
+		
+
+		EntityManager em = EMSingleton.getInstance().createEntityManager();
+		//StoredProcedureQuery storedProcedure = em.createStoredProcedureQuery("sales_tax");
+		//storedProcedure.execute();
+		String query = "select FUNCTION('FNC_CALCULO') from dual";
+		TypedQuery<String> emps = em.createQuery(query, String.class);
+		System.out.println(emps.getResultList());
 	}
 
 }
